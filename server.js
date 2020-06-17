a03f4c9f49ce5d828e0281ffab64ef67030a911a
Tushar Dhuri <tushar.dhuri@aon.com>
2020-06-17 23:22:30 +0100

    Creating express server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+//common js
+
+const app = express();
+
+app.get('/',(req,res)=> res.json({msg:'Welcome to Customer Storage API'}));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
